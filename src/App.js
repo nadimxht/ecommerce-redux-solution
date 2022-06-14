c540a3c1e4c1262499a5ef2bae832ff4aa3aafd7
nadimxht <nadimxht@gmail.com>
2022-06-14 18:10:23 +0100

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import React from 'react'
-import {useEffect,useState} from 'react'
-import axios from 'axios'
-import ListProducts from './components/ListProducts';
-import {useSelector} from 'react-redux'
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList.js';
 
-import Cart from './components/Cart';
 function App() {
-  const [products,setProducts]=useState([]);
-  const [loader,setLoader]=useState(true);
-  const ProductsInCart = useSelector(state=>state)
-  useEffect(()=>{
-    const getData = async()=>{
-      await axios.get("https://fakestoreapi.com/products").then((response)=>{
-        setProducts(response.data);
-        setLoader(false);
-      }
-  ).catch((err)=>
-  console.log(err))
-    }
-  getData();
-  },[])
-
   return (
-    <div className="App container-fluid">
-      {
-        loader ? 'loading' : <ListProducts  data={products} />
-      }
-      <br></br>
-      <h1>Cart :</h1>
-      <Cart products={ProductsInCart}/>
-      
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import Store from './redux/Store'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <Provider store={Store}>
+ 
     <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>
+
   ,
   document.getElementById('root')
 );
